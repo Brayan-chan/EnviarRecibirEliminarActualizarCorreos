fd04e584895ff55b19948c5903a1fe457297721d
brayan-chan <chanpachecob@gmail.com>
2023-05-11 17:15:09 -0600

    Cambios Version 1

---
+++ eXample/src/example/EXample.java
@@ -0,0 +1,17 @@
+package example;
+
+/**
+ *
+ * @author chanp
+ */
+public class EXample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Esto es un ejemplo de repos");
+    }
+    
+}
